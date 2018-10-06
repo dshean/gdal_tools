8db321f7b64bf6f7e85db75804437ae999d1860a
David Shean <dshean@gmail.com>
2018-10-05 23:37:32 -0700

    get_extent: added

---
+++ get_extent.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+""" 
+Return bounding box extent for input raster
+xmin ymin xmax ymax
+
+Useful for gdalwarp -te and warptool.py
+"""
+
+import sys
+from osgeo import gdal
+from pygeotools.lib import geolib
+
+fn = sys.argv[1]
+ds = gdal.Open(fn)
+extent = geolib.ds_extent(ds)
+#extent_geom = geolib.ds_geom_extent(ds)
+print ' '.join(map(str, extent))
+#print ' '.join(map(str, extent_geom))
