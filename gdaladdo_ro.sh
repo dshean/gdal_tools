01476b3e56b975d8fa323450a2304f5ca2ca338a
David Shean <dshean@gmail.com>
2018-11-22 14:35:26 -0800

    gdaladdo_ro: add note

--- gdaladdo_ro.sh
+++ gdaladdo_ro.sh
@@ -9,3 +9,6 @@ r=average
 
 #Run in parallel for all inputs
 parallel --bar "gdaladdo -q -ro -r $r --config COMPRESS_OVERVIEW LZW --config BIGTIFF_OVERVIEW YES {} 2 4 8 16 32 64" ::: "$@" 
+
+#Add option for recursive overview generation
+#Build first overview, then loop with that as input, always doing level 2
